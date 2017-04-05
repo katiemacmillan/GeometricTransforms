86d67acefbeb2d88164bee94ff411370196bf32e
Katie MacMillan <wanderer86lost@gmail.com>
2017-04-05 10:38:24 -0600

    added in splash, ip.lua and initial set up for ui framework. Includes Weiss Rotate and Scale functions

---
+++ prog3.lua
@@ -0,0 +1,49 @@
+--[[
+
+  * * * * lip.lua * * * *
+
+Lua image processing demo program: exercise all LuaIP library routines.
+
+Authors: John Weiss and Alex Iverson
+Class: CSC442/542 Digital Image Processing
+Date: Spring 2017
+
+--]]
+
+-- LuaIP image processing routines
+require "ip"   -- this loads the packed distributable
+local viz = require "visual"
+local il = require "il"
+-- for k,v in pairs( il ) do io.write( k .. "\n" ) end
+
+-- load images listed on command line
+local imgs = {...}
+for i, fname in ipairs( imgs ) do loadImage( fname ) end
+
+-----------
+-- menus --
+-----------
+
+local cmarg1 = {name = "color model", type = "string", displaytype = "combo", choices = {"rgb", "yiq", "ihs"}, default = "rgb"}
+local cmarg2 = {name = "color model", type = "string", displaytype = "combo", choices = {"yiq", "yuv", "ihs"}, default = "yiq"}
+local cmarg3 = {name = "interpolation", type = "string", displaytype = "combo", choices = {"nearest neighbor", "bilinear"}, default = "bilinear"}
+
+imageMenu("Weiss",
+  {
+    {"Resize", il.rescale,
+      {{name = "rows", type = "number", displaytype = "spin", default = 1024, min = 1, max = 16384},
+       {name = "cols", type = "number", displaytype = "spin", default = 1024, min = 1, max = 16384}, cmarg3}},
+    {"Rotate", il.rotate,
+      {{name = "theta", type = "number", displaytype = "slider", default = 0, min = -360, max = 360}, cmarg3}},
+  }
+)
+
+-- help menu
+imageMenu("Help",
+  {
+    {"Help", viz.imageMessage("Help", "Under the File menu a user can open a new image, save the current image or exit the program.\n\nBy right clicking on the image tab, the user can duplicate or reload the image. The user may also press Ctrl+D to duplicate an image or Crtl+R to reload it.\n\nThere are multiple menus from which to choose. You are able to do some preprocessing with a few point processes. Then edge detectors and other neighborhood processes which have been implemented may be run.\n\nThe user can also add noise to better show the effect of the noise cleaning filters.")},
+    {"About", viz.imageMessage("Lua Image Neighborhood Processing" .. viz.VERSION, "Authors: Forrest Miller and Katie MacMillan\nClass: CSC442 Digital Image Processing\nDate: March 14th, 2017")},
+  }
+)
+
+start()
